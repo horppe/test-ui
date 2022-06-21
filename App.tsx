9c33dafa5a2ff48f4d4bb049f906730bf492ff50
Samson <samson.oluokun@omnibiz.com>
2022-06-21 11:41:56 +0100

    linked fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import Navigations from './src/navigation/RootNavigation';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Navigations />
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
